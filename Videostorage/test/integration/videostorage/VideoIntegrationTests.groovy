857bdde3c51eb2120914f238bb731e37ecbd2ff9
Saran Siripuekpong <anti7gn@gmail.com>
2013-05-19 17:23:18 +0700

    Add Integration Test

---
+++ Videostorage/test/integration/videostorage/VideoIntegrationTests.groovy
@@ -0,0 +1,22 @@
+package videostorage
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class VideoIntegrationTests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testSomething() {
+        fail "Implement me"
+    }
+}
